1c7eee6d6779f464097bd68146c4824765f3b535
Boitumelo Ruf <Boitumelo.Ruf@iosb.fraunhofer.de>
2023-12-07 10:43:09 +0000

    Adjusted FindAravis to aravis-0.8

--- camera_aravis/CMakeModules/FindAravis.cmake
+++ camera_aravis/CMakeModules/FindAravis.cmake
@@ -1,28 +1,23 @@
+# Set Aravis version to finde
+set(ARAVIS_VERSION 0.8)
 
-find_package(PkgConfig QUIET)
+find_path(Aravis_INCLUDE_DIRS arv.h
+  PATHS
+  "$ENV{ARAVIS_INCLUDE_PATH}"
+  /usr/local/include/aravis-${ARAVIS_VERSION}
+  /usr/include/aravis-${ARAVIS_VERSION}
+)
 
-if( PKG_CONFIG_FOUND )
-  pkg_check_modules( Aravis aravis-0.6 )
-endif()
+find_library(Aravis_LIBRARIES aravis-${ARAVIS_VERSION}
+  PATHS
+  "$ENV{ARAVIS_LIBRARY}"
+  /usr/local/lib
+  /usr/lib
+  /usr/lib/x86_64-linux-gnu
+)
 
-if( NOT Aravis_FOUND )
-  message("Aravis (aravis-0.6) could not be found by pkg-config. Trying to manually find Aravis.")
-  find_path(Aravis_INCLUDE_DIRS arv.h
-    PATHS
-    "$ENV{ARAVIS_INCLUDE_PATH}"
-    /usr/local/include/aravis-0.6
-    /usr/include/aravis-0.6
-  )
-  find_library(Aravis_LIBRARIES aravis-0.6
-    PATHS
-    "$ENV{ARAVIS_LIBRARY}"
-    /usr/local/lib
-    /usr/lib
-    /usr/lib/x86_64-linux-gnu
-  )
-  include(FindPackageHandleStandardArgs)
-  find_package_handle_standard_args( Aravis DEFAULT_MSG
-    Aravis_INCLUDE_DIRS
-    Aravis_LIBRARIES
-  )
-endif()
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args( Aravis DEFAULT_MSG
+  Aravis_INCLUDE_DIRS
+  Aravis_LIBRARIES
+)
